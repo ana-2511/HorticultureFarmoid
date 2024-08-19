d6d80b953c865d4e220db35a6d5ce099b04b66b4
ana-2511 <130639950+ana-2511@users.noreply.github.com>
2024-08-20 02:24:16 +0530

    hortiapp.py

--- hortiapp.py
+++ hortiapp.py
@@ -110,7 +110,7 @@ def main():
         if st.button(t('Submit')):
             if username:
                 st.session_state['username'] = username
-                st.experimental_rerun()
+                st.rerun()
             else:
                 st.warning(t('Please enter a username.'))
     else:
@@ -119,7 +119,7 @@ def main():
         # Add a logout button
         if st.button(t('Logout')):
             st.session_state['username'] = ''
-            st.experimental_rerun()
+            st.rerun()
 
         st.markdown("---")
 
